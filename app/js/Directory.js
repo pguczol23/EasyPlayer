66e1599484788da27f32429d83cf1621c75fd11f
pguczol23 <38167698+pguczol23@users.noreply.github.com>
2019-06-30 21:31:32 +0300

    Directory.js

--- app/js/Directory.js
+++ app/js/Directory.js
@@ -1,17 +1,38 @@
 class Directory {
-    static Exists(dir) {
-        return require('fs').existsSync(dir);
+    static Exists(path) {
+        return require('fs').existsSync(path);
     }
-    static GetAllFiles(dir) {
+    static GetAllFiles(path, isFullPath) {
         const fs = require('fs');
-        fs.readdir(dir, (err, files) => {
-            if (err) return err;
-            return files;
+        var files = [];
+        fs.readdirSync(path).forEach(file => {
+            if (!Directory.isDirectory(path + '\\' + file)) {
+                if (isFullPath) {
+                    files.push(path + '\\' + file);
+                } else {
+                    files.push(file);
+                }
+            }
         });
+        return files;
     }
-    static isDirectory(dir) {
-        if (Directory.Exists(dir)) {
-            return require('fs').lstatSync(dir).isDirectory();
+    static GetAllDirectories(path, isFullPath) {
+        const fs = require('fs');
+        var dirs = [];
+        fs.readdirSync(path).forEach(dir => {
+            if (Directory.isDirectory(path + '\\' + dir)) {
+                if (isFullPath) {
+                    dirs.push(path + '\\' + dir);
+                } else {
+                    dirs.push(dir);
+                }
+            }
+        });
+        return dirs;
+    }
+    static isDirectory(path) {
+        if (Directory.Exists(path)) {
+            return require('fs').lstatSync(path).isDirectory();
         }
     }
 }
